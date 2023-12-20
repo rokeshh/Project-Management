45966191b7c2c0f88661405d6ea6ac2564a2fc8e
Rakesh <114012432+mrrowbot@users.noreply.github.com>
2023-12-20 13:20:58 +0530

    Your descriptive commit message here

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,15 @@ import {useSelector} from 'react-redux';
 const Navbar = () => {
 
   const {tasksList} = useSelector((state) => state.tasks)
+  // hekkkk
 
   return (
     <>
       <h1 className="text-center my-4 text-primary">Project Management</h1>
       <p className="text-center lead">{`Currently ${tasksList.length} task(s) pending`}</p>
+
     </>
+    
   );
 };
 
